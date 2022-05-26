819bf991151919de41e4227671eb0e2ff4a19757
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-05-26 19:17:05 +0600

    Details Part

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 
@@ -24,8 +25,8 @@ async function run() {
         const itemCollection = client.db("sportSwear").collection("inventory");
         const reviewCollection = client.db("sportSwear").collection("review");
 
-         // Loading all the inventories
-        app.get('/inventory', async(req, res) => {
+        // Loading all the inventories
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
             const inventory = await cursor.toArray();
@@ -33,14 +34,20 @@ async function run() {
         })
 
         // Loading all the rivews
-        app.get('/review', async(req, res) => {
+        app.get('/review', async (req, res) => {
             const query = {};
             const cursor = reviewCollection.find(query);
             const review = await cursor.toArray();
             res.send(review);
         })
 
-
+        // Loading a particular inventory { Load inventory:id details }
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await itemCollection.findOne(query);
+            res.json(inventory);
+        });
        
     }
     finally {
